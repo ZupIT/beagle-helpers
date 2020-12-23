b6bf6ab15f3c8b20ee4907d3e5d5010e33d91e79
maximo <luis.maximo@zup.com.br>
2020-12-23 15:05:01 -0300

    init backend project

---
+++ BeagleScaffold/backend/src/main/kotlin/br/com/zup/bff/exception/ExceptionHandler.kt
@@ -0,0 +1,16 @@
+package br.com.zup.bff.exception
+
+import br.com.zup.bff.dto.response.ErrorResponse
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@RestControllerAdvice
+class ExceptionHandler() : ResponseEntityExceptionHandler() {
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+@ExceptionHandler(Exception::class)
+    fun unexpectedException(exception:Exception)= ErrorResponse("Oops.. Something with wrong!")
+}
