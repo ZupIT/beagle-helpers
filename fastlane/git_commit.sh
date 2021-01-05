1f04a39ada7afe788753ee877fa7b85f2efeb234
carlossteinzup <carlos.stein@zup.com.br>
2021-01-05 16:06:25 -0300

    updating fastlane config

--- fastlane/git_commit.sh
+++ fastlane/git_commit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ -n $(git status --porcelain) ]]; then
-    git config user.email 'beagle@zup.com.br'
-    git config user.name 'Beagle'
+    git config user.email 'hernand.azevedo@zup.com.br'
+    git config user.name 'Hernand Azevedo'
     git add "$2"
     git commit -sm "$1"
 fi
