f08d739679e427180e5162170def15873e5a515b
Victor Rabelo <victor.rabelo@zup.com.br>
2021-01-06 18:03:42 -0300

    fixed workspace and CD

---
+++ iOS/Pods/BeagleDefaults/README.md
@@ -0,0 +1,3 @@
+# Beagle-helpers
+
+This is a repository to organize several auxiliary frameworks/libs made by our team.
---
+++ iOS/Pods/Target Support Files/Pods-BeagleDefaults/Pods-BeagleDefaults-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BeagleDefaults : NSObject
+@end
+@implementation PodsDummy_Pods_BeagleDefaults
+@end
