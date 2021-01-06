aa67812c5e24f39e33f23283d5704273ff420895
jacoabreuzup <joao.abreu@zup.com.br>
2021-01-06 18:16:30 -0300

    applying gitignore

--- iOS/Pods/Target Support Files/BeagleScaffold/BeagleScaffold-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BeagleScaffold : NSObject
-@end
-@implementation PodsDummy_BeagleScaffold
-@end
--- iOS/Pods/Target Support Files/Pods-BeagleScaffold/Pods-BeagleScaffold-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BeagleScaffold : NSObject
-@end
-@implementation PodsDummy_Pods_BeagleScaffold
-@end
