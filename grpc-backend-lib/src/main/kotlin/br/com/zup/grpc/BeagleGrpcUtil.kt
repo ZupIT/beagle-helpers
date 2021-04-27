f98238b44b83f1f90637167b9f7cbbacd76184fa
Gabriel Maximo <luis.maximo@zup.com.br>
2021-04-27 18:00:43 -0300

    test

--- grpc-backend-lib/src/main/kotlin/br/com/zup/grpc/BeagleGrpcUtil.kt
+++ grpc-backend-lib/src/main/kotlin/br/com/zup/grpc/BeagleGrpcUtil.kt
@@ -2,44 +2,45 @@ package br.com.zup.grpc
 
 import beagle.Messages
 import br.com.zup.beagle.serialization.jackson.BeagleSerializationUtil
+import br.com.zup.beagle.widget.layout.Screen
 import com.fasterxml.jackson.annotation.JsonInclude
 
 private fun asJsonString(value: Any): String {
     return BeagleSerializationUtil.beagleObjectMapper().writeValueAsString(value)
 }
 
-private fun buildDataContext(context: Map<String, Any>): Messages.DataContext {
-    val messageBuilder = Messages.DataContext.newBuilder().setId(context["id"] as String)
+private fun buildDataContext(context: Map<String, Screen>): Messages.DataContext {
+    val messageBuilder = Messages.DataContext.newBuilder().setId(context["id"].toString())
     if (context["value"] != null) {
         messageBuilder.value = asJsonString(context["value"]!!)
     }
     return messageBuilder.build()
 }
 
-private fun serializeNodeAttributes(jsonMap: MutableMap<String, Any>): Map<String, Any> {
+private fun serializeNodeAttributes(jsonMap: MutableMap<String, Screen>): Map<String, Screen> {
     val skip = listOf("_beagleComponent_", "context", "id", "style", "children", "child")
-    val attributes = HashMap<String, Any>()
+    val attributes = HashMap<String, Screen>()
     jsonMap.forEach {
         if (!skip.contains(it.key)) attributes[it.key] = it.value
     }
     return attributes
 }
 
-private fun buildNode(jsonMap: MutableMap<String, Any>): Messages.ViewNode {
+private fun buildNode(jsonMap: MutableMap<String, Screen>): Messages.ViewNode {
     val messageBuilder = Messages
         .ViewNode
         .newBuilder()
-        .setBeagleComponent(jsonMap["_beagleComponent_"] as String)
+        .setBeagleComponent(jsonMap["_beagleComponent_"].toString())
 
-    if (jsonMap["id"] != null) messageBuilder.id = jsonMap["id"] as String
-    if (jsonMap["context"] != null) messageBuilder.context = buildDataContext(jsonMap["context"] as Map<String, Any>)
+    if (jsonMap["id"] != null) messageBuilder.id = jsonMap["id"].toString()
+    if (jsonMap["context"] != null) messageBuilder.context = buildDataContext(jsonMap["context"] as Map<String, Screen>)
     if (jsonMap["style"] != null) messageBuilder.style = asJsonString(jsonMap["style"]!!)
-    if (jsonMap["child"] != null) messageBuilder.child = buildNode(jsonMap["child"] as MutableMap<String, Any>)
+    if (jsonMap["child"] != null) messageBuilder.child = buildNode(jsonMap["child"] as MutableMap<String, Screen>)
 
     val attributes = serializeNodeAttributes(jsonMap)
     if (attributes.isNotEmpty()) messageBuilder.attributes = asJsonString(attributes)
 
-    val children = jsonMap["children"] as List<MutableMap<String, Any>>?
+    val children = jsonMap["children"] as List<MutableMap<String, Screen>>?
     if (children?.isEmpty() == false) {
         children.forEach {
             messageBuilder.addChildren(buildNode(it))
@@ -49,10 +50,10 @@ private fun buildNode(jsonMap: MutableMap<String, Any>): Messages.ViewNode {
     return messageBuilder.build()
 }
 
-fun asGrpcView(widget: Any): Messages.ViewNode {
+fun asGrpcView(widget: Screen): Messages.ViewNode {
     val mapper = BeagleSerializationUtil.beagleObjectMapper()
     mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
-    val jsonMap = mapper.convertValue(widget, MutableMap::class.java) as MutableMap<String, Any>
+    val jsonMap = mapper.convertValue(widget, MutableMap::class.java) as MutableMap<String, Screen>
     try {
         return buildNode(jsonMap)
     } catch (err: Throwable) {
