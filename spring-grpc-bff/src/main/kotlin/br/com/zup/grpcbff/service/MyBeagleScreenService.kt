04834d01cfc7484f5ae429ae8c93046ba508450b
Gabriel Maximo <luis.maximo@zup.com.br>
2021-05-03 18:11:53 -0300

    add example to get a current context headers

--- spring-grpc-bff/src/main/kotlin/br/com/zup/grpcbff/service/MyBeagleScreenService.kt
+++ spring-grpc-bff/src/main/kotlin/br/com/zup/grpcbff/service/MyBeagleScreenService.kt
@@ -1,7 +1,7 @@
 package br.com.zup.grpcbff.service
 
 import br.com.zup.beagle.widget.layout.ScreenBuilder
-import br.com.zup.grpc.BeagleHeadersInterceptor
+import br.com.zup.grpcbff.interceptor.BeagleHeadersInterceptor
 import br.com.zup.grpc.BeagleScreenService
 import net.devh.boot.grpc.server.service.GrpcService
 import br.com.zup.grpcbff.screen.HomeScreen
@@ -10,6 +10,8 @@ import br.com.zup.grpcbff.screen.SplashScreen
 @GrpcService(interceptors = [BeagleHeadersInterceptor::class])
 class MyBeagleAppService: BeagleScreenService() {
     override fun screens(): Map<String, (String) -> ScreenBuilder> {
+        val requestHeaders = BeagleHeadersInterceptor.HEADERS_VALUE.get()
+
         return mapOf(
             "home" to { params -> HomeScreen(params) },
             "splash" to { SplashScreen },
