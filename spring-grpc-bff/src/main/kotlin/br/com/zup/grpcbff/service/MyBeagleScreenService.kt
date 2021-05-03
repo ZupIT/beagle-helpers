1d8596b560e720a543c7eebcf4c0431114763168
Gabriel Maximo <luis.maximo@zup.com.br>
2021-05-03 10:58:29 -0300

    fix service example

--- spring-grpc-bff/src/main/kotlin/br/com/zup/grpcbff/service/MyBeagleScreenService.kt
+++ spring-grpc-bff/src/main/kotlin/br/com/zup/grpcbff/service/MyBeagleScreenService.kt
@@ -4,16 +4,15 @@ import br.com.zup.beagle.widget.layout.ScreenBuilder
 import br.com.zup.grpc.BeagleHeadersInterceptor
 import br.com.zup.grpc.BeagleScreenService
 import net.devh.boot.grpc.server.service.GrpcService
-import br.com.zup.grpcbff.screen.ButtonScreen
-import br.com.zup.grpcbff.screen.TextScreen
-import io.grpc.Context
+import br.com.zup.grpcbff.screen.HomeScreen
+import br.com.zup.grpcbff.screen.SplashScreen
 
 @GrpcService(interceptors = [BeagleHeadersInterceptor::class])
 class MyBeagleAppService: BeagleScreenService() {
-    override fun screens(): Map<String, (String?) -> ScreenBuilder> {
+    override fun screens(): Map<String, (String) -> ScreenBuilder> {
         return mapOf(
-            "button" to { ButtonScreen },
-            "text" to { params -> TextScreen(params) },
+            "home" to { params -> HomeScreen(params) },
+            "splash" to { SplashScreen },
         )
     }
 }
