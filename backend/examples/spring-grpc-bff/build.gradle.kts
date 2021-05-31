ba2c75c201c0327234ab93af09651742f3155979
Gabriel Maximo <luis.maximo@zup.com.br>
2021-05-31 18:32:07 -0300

    fix gradle.build on root dir

--- backend/examples/spring-grpc-bff/build.gradle.kts
+++ backend/examples/spring-grpc-bff/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.5"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm")
+	kotlin("jvm") version "1.4.31"
 	kotlin("plugin.spring") version "1.4.32"
 	application
 }
@@ -15,7 +15,7 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("br.com.zup.beagle:beagle-grpc-backend:0.0.2-maximo")
+	implementation("br.com.zup.beagle:beagle-grpc-backend:0.0.1")
 	implementation("net.devh:grpc-server-spring-boot-starter:2.11.0.RELEASE")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
