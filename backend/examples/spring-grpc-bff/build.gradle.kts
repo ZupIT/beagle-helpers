609d7ed804b341bea79a8dfbb7c891c440606aa7
Gabriel Maximo <luis.maximo@zup.com.br>
2021-05-31 16:11:11 -0300

    fix gradle modules and publishing steps

--- backend/examples/spring-grpc-bff/build.gradle.kts
+++ backend/examples/spring-grpc-bff/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.5"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.4.32"
+	kotlin("jvm")
 	kotlin("plugin.spring") version "1.4.32"
 	application
 }
@@ -12,15 +12,10 @@ group = "br.com.zup"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
-repositories {
-	mavenCentral()
-	mavenLocal()
-}
-
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("br.com.zup:beagle-grpc-backend:0.0.1")
+	implementation("br.com.zup.beagle:beagle-grpc-backend:0.0.1")
 	implementation("net.devh:grpc-server-spring-boot-starter:2.11.0.RELEASE")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
